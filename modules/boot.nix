0c8b06dc5369a7bef9b505e148db1a99f8e8a3f4
PuppiestDoggo <blackmine57@protonmail.com>
2025-03-23 14:11:33 +0100

    clean tmpdir

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,4 +6,5 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.luks.devices."luks-d365a782-4769-4b81-a32b-ceaacb8f76de".device = "/dev/disk/by-uuid/d365a782-4769-4b81-a32b-ceaacb8f76de";
+  boot.cleanTmpDir = true;
 }
