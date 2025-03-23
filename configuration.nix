0c77b764722390cb6cea13ea91d960d6dd1345bc
blackmine57 <blackmine57@protonmail.com>
2025-03-23 19:45:06 +0100

    feat: use label+i forgor

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     [
       ./hardware-configuration.nix
       ./modules/users.nix
-      ./modules/nvidia.nix
+#      ./modules/nvidia.nix
       ./modules/locale.nix
       ./modules/packages.nix
       ./modules/network.nix
--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,41 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos-root";
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-nixos-root".device = "/dev/disk/by-label/nixos-root";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/EFI";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp42s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-d365a782-4769-4b81-a32b-ceaacb8f76de".device = "/dev/disk/by-uuid/d365a782-4769-4b81-a32b-ceaacb8f76de";
   boot.tmp.cleanOnBoot = true;
-  boot.kernel.sysctl = { "vm.swappiness" = 5;};
+  boot.kernel.sysctl = { "vm.swappiness" = 5; };
 }
