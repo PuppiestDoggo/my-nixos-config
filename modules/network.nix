a3e4bf5cef24b2141ff7ff34b683b4ec53330d56
PuppiestDoggo <blackmine57@protonmail.com>
2025-03-23 16:23:05 +0100

    feat: dns

--- modules/network.nix
+++ modules/network.nix
@@ -2,5 +2,7 @@
 {
   networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
+  networking.nameservers = ["80.67.169.12" "80.67.169.40" "1.1.1.1" "8.8.8.8"];
+
 
 }
