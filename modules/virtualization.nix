5463045e720dddf97f3c252e7a52967aed856773
PuppiestDoggo <blackmine57@protonmail.com>
2025-03-23 13:34:01 +0100

    feat: KVM

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 {
+  # Enable virt-manager
+  programs.virt-manager.enable = true;
+
+  # Add the user 'arnaud' to the libvirtd group for virtualization permissions
+  users.groups.libvirtd.members = [ "arnaud" ];
+
+  # Enable the libvirtd service for virtualization support
+  virtualisation.libvirtd.enable = true;
+
+  # Enable SPICE USB redirection for virtual machines
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  # Enable VMware host support
   virtualisation.vmware.host.enable = true;
 }
-
