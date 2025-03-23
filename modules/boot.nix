74d815d325a7b96cc1fbaf0ba039d619024a3dab
PuppiestDoggo <blackmine57@protonmail.com>
2025-03-23 16:02:28 +0100

    did some weird git stuff, but basically revert + swappiness

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,27 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  boot.kernel.sysctl = { "vm.swappiness" = 5;};
-  # Bootloader configuration
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Clean temporary directories on boot
+  boot.initrd.luks.devices."luks-d365a782-4769-4b81-a32b-ceaacb8f76de".device = "/dev/disk/by-uuid/d365a782-4769-4b81-a32b-ceaacb8f76de";
   boot.tmp.cleanOnBoot = true;
-  environment.etc.crypttab.text = ''
-    HDD UUID=abd2098c-29c7-41b1-9b16-0ed2e347e5b6 none luks
-  '';
-
-
-  # Filesystem configuration for HDD
-  fileSystems."/mnt/HDD" = {
-    device = "/dev/mapper/HDD";
-    fsType = "xfs";
-    options = [
-      "defaults"
-      "noatime"
-      "x-systemd.automount"
-      "x-systemd.device-timeout=5"
-    ];
-  };
+  boot.kernel.sysctl = { "vm.swappiness" = 5;};
 }
