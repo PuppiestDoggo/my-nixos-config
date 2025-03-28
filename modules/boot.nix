e771c6534722e38f6e58e0aa023cc0bed5247667
PuppiestDoggo <blackmine57@protonmail.com>
2025-03-28 17:16:22 +0100

    bruh

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,4 +7,5 @@
 
   boot.tmp.cleanOnBoot = true;
   boot.kernel.sysctl = { "vm.swappiness" = 5; };
+  boot.initrd.luks.devices."luks-d365a782-4769-4b81-a32b-ceaacb8f76de".device = "/dev/disk/by-uuid/d365a782-4769-4b81-a32b-ceaacb8f76de";
 }
